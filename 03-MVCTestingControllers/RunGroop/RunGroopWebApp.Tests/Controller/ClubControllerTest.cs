c482e800a1f8d1d73f405b9621c7c63d5b76bdd9
Ahmed Taha <ahmadhamidty@gmail.com>
2023-07-23 11:47:43 +0200

    Updating connection string and sperate files

---
+++ 03-MVCTestingControllers/RunGroop/RunGroopWebApp.Tests/Controller/ClubControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using FakeItEasy;
+using RunGroopWebApp.Interfaces;
+
+
+namespace RunGroopWebApp.Tests.Controller;
+public class ClubControllerTest
+{
+    private IClubRepository _clubRepository;
+    private IPhotoService _photoService;
+    public ClubControllerTest()
+    {
+        //DI
+        _clubRepository = A.Fake<IClubRepository>();
+        _photoService = A.Fake<IPhotoService>();
+    }
+
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
