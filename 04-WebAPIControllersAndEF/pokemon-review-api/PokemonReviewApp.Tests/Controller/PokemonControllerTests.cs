3b1f27e74a6324eb04834a4448b1ad678304fd66
Ahmed Taha <ahmadhamidty@gmail.com>
2023-08-05 03:14:58 +0300

    07 and 08 - Testing EndPoints and EF repoistory

---
+++ 04-WebAPIControllersAndEF/pokemon-review-api/PokemonReviewApp.Tests/Controller/PokemonControllerTests.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Controllers;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonReviewApp.Tests.Controller;
+public class PokemonControllerTests
+{
+    private readonly IPokemonRepository _pokemonRepository;
+    private readonly IReviewRepository _reviewerRepository;
+    private readonly IMapper _mapper;
+    public PokemonControllerTests()
+    {
+        _pokemonRepository = A.Fake<IPokemonRepository>();
+        _reviewerRepository = A.Fake<IReviewRepository>();
+        _mapper = A.Fake<IMapper>();
+    }
+
+    [Fact]
+    public void PokemonController_GetPokemons_ReturnOk()
+    {
+        //Arrange 
+        /*
+         Because the repository method returns ICollection: 
+                ICollection<Pokemon> GetPokemons();
+         */
+        var pokemons = A.Fake<ICollection<PokemonDto>>();
+        /*
+         And because we are mapping the ICollection to List
+         */
+        var pokemonList = A.Fake<List<PokemonDto>>();
+        //Mapping
+        A.CallTo(() =>
+        _mapper.Map<List<PokemonDto>>(pokemons)).Returns(pokemonList);
+
+        var controller = new PokemonController
+            (_pokemonRepository, _reviewerRepository,
+            _mapper);
+
+        //Act 
+        var result = controller.GetPokemons();
+
+        //Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType(typeof(OkObjectResult));
+
+    }
+
+    [Fact]
+    public void PokemController_CreatePokemon_ReturnOk()
+    {
+        //Arrange 
+        int ownerId = 1;
+        int catId = 2;
+        var pokemon = A.Fake<Pokemon>();
+        var pokemonCreate = A.Fake<PokemonDto>();
+        var pokemons = A.Fake<ICollection<PokemonDto>>();
+        var pokemonList = A.Fake<IList<PokemonDto>>();
+
+        A.CallTo(() => _pokemonRepository.
+        GetPokemonTrimToUpper(pokemonCreate)).Returns(pokemon);
+
+        A.CallTo(() => _mapper.Map<Pokemon>(pokemonCreate))
+            .Returns(pokemon);
+
+        A.CallTo(() => _pokemonRepository.
+        CreatePokemon(ownerId, catId, pokemon)).Returns(true);
+
+        var controller = new PokemonController
+            (_pokemonRepository, _reviewerRepository,
+            _mapper);
+
+
+        //Act
+        var result = controller.
+            CreatePokemon(ownerId,catId,pokemonCreate);
+        
+        //Assert
+        result.Should().NotBeNull();
+    }   
+
+}
---
+++ 04-WebAPIControllersAndEF/pokemon-review-api/PokemonReviewApp.Tests/Repositroy/PokemonRepositoryTests.cs
@@ -0,0 +1,81 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonReviewApp.Tests.Repositroy;
+public class PokemonRepositoryTests
+{
+    private async Task<DataContext> GetDatabaseContext()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var databaseContext = new DataContext(options);
+        databaseContext.Database.EnsureCreated();
+        if (await databaseContext.Pokemon.CountAsync() <= 0)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                databaseContext.Pokemon.Add(
+                new Pokemon()
+                {
+                    Name = "Pikachu",
+                    BirthDate = new DateTime(1903, 1, 1),
+                    PokemonCategories = new List<PokemonCategory>()
+                        {
+                                new PokemonCategory { Category = new Category() { Name = "Electric"}}
+                        },
+                    Reviews = new List<Review>()
+                        {
+                                new Review { Title="Pikachu",Text = "Pickahu is the best pokemon, because it is electric", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Teddy", LastName = "Smith" } },
+                                new Review { Title="Pikachu", Text = "Pickachu is the best a killing rocks", Rating = 5,
+                                Reviewer = new Reviewer(){ FirstName = "Taylor", LastName = "Jones" } },
+                                new Review { Title="Pikachu",Text = "Pickchu, pickachu, pikachu", Rating = 1,
+                                Reviewer = new Reviewer(){ FirstName = "Jessica", LastName = "McGregor" } },
+                        }
+                });
+                await databaseContext.SaveChangesAsync();
+            }
+        }
+        return databaseContext;
+    }
+
+    [Fact]
+    public async void PokemonRepository_GetPokemon_ReturnsPokemons()
+    {
+        //Arrange 
+        var name = "Pikachu";
+        var dbContext = await GetDatabaseContext();
+        var pokemonRepo = new PokemonRepository(dbContext);
+
+        //Act 
+        var pokemon = pokemonRepo.GetPokemon(name);
+
+        //Assert
+        pokemon.Should().NotBeNull();
+        pokemon.Should().BeOfType<Pokemon>();
+    }
+     
+    [Theory]
+    [InlineData(1)]
+    public async void PokemonRepository_GetPokemonRating_ReturnsDecimal(int pokeId)
+    {
+        //Arrange 
+        var dbContext = await GetDatabaseContext();
+        var pokeRepository = new PokemonRepository(dbContext);
+        decimal sure = default;
+        //Act 
+        var pokeRating = pokeRepository.GetPokemonRating(pokeId);
+        //Assert
+        pokeRating.Should().NotBe(0);
+        pokeRating.Should().BeInRange(1, 10);
+    }
+}
